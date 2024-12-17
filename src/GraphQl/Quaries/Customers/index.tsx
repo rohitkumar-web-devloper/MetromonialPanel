455738458f18ee6c46dc4d4b72232ebf23e43acb
Tushar Gola <golat8602gmail.com>
2024-12-17 21:18:35 +0530

    fix gpl syntax

--- src/GraphQl/Quaries/Customers/index.tsx
+++ src/GraphQl/Quaries/Customers/index.tsx
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 export const CUSTOMERS_GET = gql`
-  query customers($page: Int, $pageSize: Int, $filter: PlanFilter) {
+  query customers($page: Int, $pageSize: Int, $filter: UserFilter) {
     customers(page: $page, pageSize: $pageSize, filter: $filter) {
         customers {
       createdAt
